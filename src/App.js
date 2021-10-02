c2787ae1d42ee4d1fd218cfcf8495f63352706b3
Mihocsa Szilard <mihocsa48@gmail.com>
2021-10-02 12:25:28 +0200

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.scss";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+export default class QuoteMachine extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      quotes: "",
+      author: "",
+      bgColor: "",
+    };
+  }
+
+  componentDidMount() {
+    this.getQuote();
+  }
+
+  getQuote = () => {
+    fetch("https://api.quotable.io/random")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            quote: result.content,
+            author: result.author,
+          });
+          console.log(result);
+        },
+        (error) => {
+          this.setState({
+            error,
+          });
+        }
+      );
+  };
+
+  randomColor = () => {
+    let letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
+
+  render() {
+    return (
+      <div>
+        {this.state.quote ? (
+          <div id="wrapper" style={{ backgroundColor: this.randomColor() }}>
+            <div id="quote-box">
+              <p id="text">{this.state.quote}</p>
+              <p id="author">- {this.state.author}</p>
+              <button
+                onClick={() => {
+                  this.getQuote();
+                  this.randomColor();
+                }}
+              >
+                New Quote
+              </button>
+            </div>
+          </div>
+        ) : (
+          <div>{this.state.error}</div>
+        )}
+        <p className="copyright">
+          Created by:&nbsp;
+          <a href="https://github.com/mihocsaszilard">mihocsaszilard</a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      </div>
+    );
+  }
 }
-
-export default App;
